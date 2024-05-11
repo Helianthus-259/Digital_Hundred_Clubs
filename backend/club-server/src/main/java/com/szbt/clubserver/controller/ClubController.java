67ab5c951319dfdf289acc165326c592740f319f
lingyi01 <x2039358295@163.com>
2024-05-11 21:32:59 +0800

    社团的简单增删查改

--- backend/club-server/src/main/java/com/szbt/clubserver/controller/ClubController.java
+++ backend/club-server/src/main/java/com/szbt/clubserver/controller/ClubController.java
@@ -4,11 +4,11 @@ import com.szbt.clubserver.service.ClubService;
 
 import lombok.extern.slf4j.Slf4j;
 
+import org.example.entity.Club;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.Collection;
 
 
 @RestController
@@ -23,8 +23,16 @@ public class ClubController {
     {
         return clubService.queryAllClubs();
     }
+    @PostMapping("/clubname")
     public Object queryClubsByName(String name){return clubService.queryClubsByName(name);}
+    @GetMapping("/club/unpass")
     public Object queryUnpassClubs(){return clubService.queryUnpassClubs();}
+    @GetMapping("/club/pass")
     public Object queryPassClubs(){return  clubService.queryPassClubs();}
-
+    @PostMapping("/add/club")
+    public Object addClub(Club club){return clubService.save(club);}
+    @PostMapping("/update/club")
+    public Object updateClub(Club club){return clubService.saveBatch((Collection<Club>) club);}
+    @DeleteMapping("/delete/club")
+    public Object deleteClub(String id){return clubService.removeById(id);}
 }
