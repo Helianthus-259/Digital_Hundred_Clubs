167b19920e6f164a0643b19560d56fc1db092806
蔡星杰 <x2039358295@163.com>
2024-05-22 17:07:37 +0800

    更新SpringCloudGateway配置

--- backend/gateway/src/main/java/com/szbt/GatewayApplication.java
+++ backend/gateway/src/main/java/com/szbt/GatewayApplication.java
@@ -1,13 +1,37 @@
 package com.szbt;
 
+import com.szbt.config.UriConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableConfigurationProperties(UriConfiguration.class)
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder, UriConfiguration uriConfiguration) {
+        String httpUri = uriConfiguration.getHttpbin();
+        return builder.routes()
+                .route(p -> p
+                        .path("/test")
+                        .filters(f -> f.addRequestHeader("Hello", "World"))
+                        .uri(httpUri))
+                .route(p -> p
+                        .host("*.circuitbreaker.com")
+                        .filters(f -> f.circuitBreaker(config -> config
+                                .setName("mycmd")
+                                .setFallbackUri("forward:/fallback")))
+                        .uri(httpUri))
+                .build();
+    }
+
 }
