ee0c512e92e38f4157414a16369274353af2edd3
朱彪 <1282464533@qq.com>
2024-05-24 17:18:10 +0800

    将formatDate封装成了公用函数

--- frontend/src/components/personalViewChildren/ParticipatedClubs.vue
+++ frontend/src/components/personalViewChildren/ParticipatedClubs.vue
@@ -250,6 +250,7 @@ import { ref } from 'vue';
 import eventEmitter from '@/utils/eventEmitter';
 import { APIEnum, APIEventEnum, RouterEventEnum, StoreEnum, StoreEventEnum } from '@/Enum';
 import mySteps from '../mySteps.vue';
+import formatDate from '@/utils'
 
 // 展示加入了的社团
 const clubsJoin = ref(store.state.userInfo.clubs)
@@ -361,30 +362,6 @@ const getClubInfos = (value, pd) => {
             }
         }
     }
-}
-
-const formatDate = (date, fmt) => {
-    if (/(y+)/.test(fmt)) {
-        fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length));
-    }
-    let o = {
-        'M+': date.getMonth() + 1,
-        'd+': date.getDate(),
-        'h+': date.getHours(),
-        'm+': date.getMinutes(),
-        's+': date.getSeconds()
-    };
-    for (let k in o) {
-        if (new RegExp(`(${k})`).test(fmt)) {
-            let str = o[k] + '';
-            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? str : padLeftZero(str));
-        }
-    }
-    return fmt;
-}
-
-const padLeftZero = (str) => {
-    return ('00' + str).substr(str.length);
 }
 
 eventEmitter.on(APIEventEnum.getClubActAndNtcSuccess, (data) => {
