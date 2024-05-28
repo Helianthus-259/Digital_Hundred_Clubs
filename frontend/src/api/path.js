19f7d8ec4e64d77d344f146c6988e27f0f16f648
董馨怡 <1850122203@qq.com>
2024-05-28 18:31:43 +0800

    path注释

--- frontend/src/api/path.js
+++ frontend/src/api/path.js
@@ -1,28 +1,57 @@
 const base = {
+    // 学生
+    // get
+    userInfo: '/api/userInfo', // 获取用户信息
+    verifyCode: '/api/verifyCode', // 验证码
+
+    // post
     login: '/api/login', // 登录
     register: '/api/register', // 注册
-    verifyCode: '/api/verifyCode', // 验证码
+    userInfoUpdate: '/api/userInfoUpdate', // 更新用户信息
+    backBoneEvaluate: '/api/backBoneEvaluate', // 骨干评优
+
+
+    // 管理员
+    // get
+    adminInfo: 'api/adminInfo',//获取管理员信息
+
+    // post
+    postAdminLogin: '/api/postAdminLogin', //管理员登录
+    adminInfoUpdate: 'api/adminInfoUpdate',//更新管理员信息
+
+
+    // 社团
+    // get
     clubsInfo: '/api/clubsInfo', // 获取所有社团信息
     clubLatestAct: '/api/clubLatestAct', // 获取最新活动
-    userInfo: '/api/userInfo', // 获取用户信息
-    userInfoUpdate: '/api/userInfoUpdate', // 更新用户信息
-    newClubApply: '/api/newClubApply', // 提交新社团申请
     clubActAndNtc: '/api/clubActAndNtc', // 获取社团活动通知
     clubIntroduction: '/api/clubIntroduction', // 获取社团简介
     clubMembers: '/api/clubMembers', // 获取社团成员
-    postAdminLogin: '/api/postAdminLogin', //管理员登录
-    adminInfo: 'api/adminInfo',//获取管理员信息
-    adminInfoUpdate: 'api/adminInfoUpdate',//获取管理员信息
-    activityInfo: '/api/activityInfo', // 获取活动信息
-    activitiesInfo: '/api/activitiesInfo', // 获取所有活动信息
     clubActivityList: '/api/clubActivityList', // 获取社团活动列表
     clubApplyList: '/api/clubApplyList', // 获取社团申请列表
+    clubEvaluateInfo: '/api/clubEvaluateInfo',// 获取社团评优所需信息
+
+    // post
     agreeClubApply: '/api/agreeClubApply', // 同意社团申请
     rejectClubApply: '/api/rejectClubApply', // 拒绝社团申请
+    newClubApply: '/api/newClubApply', // 提交新社团申请
+
+
+    // 活动
+    // get
+    activityInfo: '/api/activityInfo', // 获取活动信息
+    activitiesInfo: '/api/activitiesInfo', // 获取所有活动信息
+
+    // post
     newActivity: '/api/newActivity', // 提交新活动
-    backBoneEvaluate: '/api/backBoneEvaluate', // 骨干评优
-    clubEvaluateInfo: '/api/clubEvaluateInfo',// 获取社团评优所需信息
+
+
+    // 文件
     uploadImage: '/api/uploadImage', // 上传图片
+
+
+
+
 }
 
 export default base;
