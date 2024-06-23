ec9a2493217f50ddf61b6fd624a220b14972333e
helianthus8 <1076753979@qq.com>
2024-06-23 10:22:05 +0800

    更新redis配置

--- backend/auth-server/src/main/java/com/szbt/authserver/config/RedisConfig.java
+++ backend/auth-server/src/main/java/com/szbt/authserver/config/RedisConfig.java
@@ -18,8 +18,6 @@ public class RedisConfig {
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(redisConnectionFactory);
-        // jackson序列化所有的类
-        Jackson2JsonRedisSerializer Jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
         // jackson序列化的一些配置
         ObjectMapper om = new ObjectMapper();
         om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
@@ -27,15 +25,14 @@ public class RedisConfig {
         Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(om, Object.class);
         // String的序列化
         StringRedisSerializer stringSerializer = new StringRedisSerializer();
-
         //将我们的key采用String的序列化方式
         template.setKeySerializer(stringSerializer);
         //将我们的hash的key也采用String的序列化方式
         template.setHashKeySerializer(stringSerializer);
         //value采用jackson序列化方式
-        template.setValueSerializer(Jackson2JsonRedisSerializer);
+        template.setValueSerializer(jackson2JsonRedisSerializer);
         //hash的value也采用jackson序列化方式
-        template.setHashValueSerializer(Jackson2JsonRedisSerializer);
+        template.setHashValueSerializer(jackson2JsonRedisSerializer);
 
         template.afterPropertiesSet();
 
