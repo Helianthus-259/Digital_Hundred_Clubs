c32e0e5167ebd256babc16fef781c643cf356602
helianthus8 <1076753979@qq.com>
2024-06-23 13:27:00 +0800

    修复数据一致性

--- backend/admin-server/src/main/java/com/szbt/adminserver/service/impl/ClubapplicationrecordServiceImpl.java
+++ backend/admin-server/src/main/java/com/szbt/adminserver/service/impl/ClubapplicationrecordServiceImpl.java
@@ -7,6 +7,7 @@ import org.example.entity.Clubapplicationrecord;
 import org.example.enums.ResultCode;
 import org.example.enums.StatusCode;
 import org.example.service.ClubClientService;
+import org.example.util.RedisKeyBuilder;
 import org.example.util.Result;
 import org.example.vo.SendMsg;
 import org.example.vo.SingleCodeVO;
@@ -88,6 +89,9 @@ public class ClubapplicationrecordServiceImpl extends ServiceImpl<Clubapplicatio
         try{
             int updateById = clubapplicationrecordMapper.updateById(clubapplicationrecord);
             if(updateById<=0) return Result.send(StatusCode.PASS_CLUB_COLLEGE_APPROVAL_ERROR,new SendMsg("学院通过社团申请失败"));
+            Clubapplicationrecord clubapplicationrecord1 = clubapplicationrecordMapper.selectById(clubapplicationrecord.getRecordId());
+            String clubKey = RedisKeyBuilder.generateClubKey(clubapplicationrecord1.getClubId());
+            clubClientService.deleteClubRedisData(clubKey);
             return Result.success(new SingleCodeVO(ResultCode.PASS_CLUB_COLLEGE_APPROVAL));
         }catch (Exception e) {
             e.printStackTrace();
