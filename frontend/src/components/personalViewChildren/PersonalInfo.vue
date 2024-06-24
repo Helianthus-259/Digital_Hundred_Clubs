49ce82c34a132264a92c60cba29f8729781d1085
NeighborLiu <2363254443@qq.com>
2024-06-24 22:25:57 +0800

    111

--- frontend/src/components/personalViewChildren/PersonalInfo.vue
+++ frontend/src/components/personalViewChildren/PersonalInfo.vue
@@ -256,7 +256,7 @@ const closeDialog = () => {
 };
 
 // 新建社团
-const departmentList = ref(store.state.departmentList)
+let departmentList = ref(store.state.departmentList)
 //在本list中会存储“校团委”的选择。这不应该由校团委审核，而是由学院管理员审核。
 departmentList = departmentList.filter(function (item) {
         return item != "校团委";
